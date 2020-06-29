4fdd597590dcf772cb9040c61787b9bf1258ca8f
Jared <38332939+jaredaritter@users.noreply.github.com>
2020-06-29 17:42:52 -0400

    testing for new item and new project functionality

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import './style.css';
-import { nav } from './nav.js';
+// import { nav } from './nav.js';
 import { header } from './header.js';
 import { item } from './item.js';
 
 const navLinks = ['Home', 'About', 'Contact'];
 const headerText = 'How Do You Do?';
 
-nav(navLinks);
+// nav(navLinks);
 header(headerText);
 item();
 
 console.log('index working');
+
+document.querySelector('.new-project').addEventListener('click', buildNewProject);
+
+function buildNewProject() {
+  const parent = document.querySelector('.content');
+  const element = document.createElement('div');
+  element.classList.add('project');
+  element.textContent = 'Dummy';
+  parent.appendChild(element);
+}
