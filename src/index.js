2b2029d8ed5fb8d1fdd3cb0b5e1d3efc59815c25
jared <38332939+jaredaritter@users.noreply.github.com>
2020-07-03 13:39:46 -0400

    remove nav references and code

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import './style.css';
-// import { nav } from './nav.js';
 import { header } from './header.js';
 import { item } from './item.js';
 
-const navLinks = ['Home', 'About', 'Contact'];
 const headerText = 'How Do You Do?';
 
-// nav(navLinks);
 header(headerText);
 item();
 
 console.log('index working');
 
-document.querySelector('.new-project').addEventListener('click', buildNewProject);
+document
+  .querySelector('.new-project')
+  .addEventListener('click', buildNewProject);
 
 function buildNewProject() {
   const parent = document.querySelector('.content');
