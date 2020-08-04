d870b373ad8856f4c9993d63c43db19208570f80
jared <38332939+jaredaritter@users.noreply.github.com>
2020-08-04 15:08:40 -0400

    Continued refactor to allow dynamic creation of projects and todo items with JS

--- README.md
+++ README.md
@@ -7,3 +7,22 @@
 - Flesh out todo items for default project
 - Create ability to make new projects
   - todos are linked to project
+
+## Planning
+
+- todos
+- project
+- separate logic and render modules
+- input information into form
+  - title
+  - description
+  - due
+  - priority
+- output into table
+  - name
+- New
+  - project
+  - todo
+- Render
+  - project
+  - todo
--- src/item.js
+++ src/item.js
@@ -1,20 +1,4 @@
 const item = (title, description, dueDate, priority) => {
-  // const getTitle = () => title;
-  // const getDescription = () => description;
-  // const getDueDate = () => dueDate;
-  // const getPriority = () => priority;
-  // const getNotes = () => notes;
-  // const getChecklist = () => checklist;
-
-  // return {
-  //   getTitle,
-  //   getDescription,
-  //   getDueDate,
-  //   getPriority,
-  //   getNotes,
-  //   getChecklist,
-  // };
-
   return {
     title,
     description,
