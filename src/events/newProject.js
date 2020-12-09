773f754963b43a941df1354346d4f3a2f94c32dc
jared <38332939+jaredaritter@users.noreply.github.com>
2020-12-09 10:47:18 -0500

    Remove unneeded console.log.

--- src/events/newProject.js
+++ src/events/newProject.js
@@ -6,7 +6,6 @@ import { clearForm } from '../helpers/clearForm';
 function newProject(e, firstProject) {
   const form = document.querySelector('#project-form');
   const formName = form.name.value;
-  console.log(form.name.value);
 
   const defaultName = 'Untitled';
   const name = formName || firstProject || defaultName;
